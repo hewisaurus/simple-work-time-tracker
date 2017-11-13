63db961e4b5cd1fc062c376c08cbeca9db1d1bb4
David Hartley <dhartley84@gmail.com>
2017-11-13 19:18:49 +1100

    Initial checkin - unconfigured

---
+++ Web/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using SimpleWorkTimeTracker.Controllers;
+
+namespace SimpleWorkTimeTracker.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(AccountController.ConfirmEmail),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+
+        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        {
+            return urlHelper.Action(
+                action: nameof(AccountController.ResetPassword),
+                controller: "Account",
+                values: new { userId, code },
+                protocol: scheme);
+        }
+    }
+}
---
+++ Web/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleWorkTimeTracker.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
