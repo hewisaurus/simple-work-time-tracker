9294689bdbe69e216cab44b8ba7d06b004b145f0
David Hartley <dhartley84@gmail.com>
2017-11-13 20:20:49 +1100

    Implemented the BCrypt class and added example server-side key

---
+++ Common/BCrypt/BCrypt.cs
@@ -0,0 +1,21 @@
+﻿using DevOne.Security.Cryptography.BCrypt;
+
+namespace Common.BCrypt
+{
+    static class BCrypt
+    {
+        private const int Rounds = 13;
+
+        public static string GeneratePasswordHash(string password, int overrideRounds = Rounds)
+        {
+            var tmpSalt = BCryptHelper.GenerateSalt(overrideRounds);
+            var hash = BCryptHelper.HashPassword(password + ServerSideKey.Value, tmpSalt);
+            return hash;
+        }
+
+        public static bool Validate(string password, string hash)
+        {
+            return BCryptHelper.CheckPassword(password + ServerSideKey.Value, hash);
+        }
+    }
+}
---
+++ Common/BCrypt/ServerSideKey.example.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.BCrypt
+{
+    //class ServerSideKey
+    //{
+    //    private const string Value = ".0jUq2eTVP85Nn8";
+    //}
+}
