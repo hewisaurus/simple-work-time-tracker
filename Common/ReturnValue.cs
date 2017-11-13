0d2ed3fb1f6d94499b3d7008fa0456fd1784c0a6
David Hartley <dhartley84@gmail.com>
2017-11-13 23:01:56 +1100

    Added ReturnValue

---
+++ Common/ReturnValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class ReturnValue
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+        public TimeSpan TimeTaken { get; set; }
+        public int Value { get; set; }
+
+        public ReturnValue()
+        {
+            Initialise(false, "", null, new TimeSpan());
+        }
+
+        public ReturnValue(bool success, string message)
+        {
+            Initialise(success, message, null, new TimeSpan());
+        }
+
+        public ReturnValue(ReturnValue returnvalue)
+        {
+            Initialise(returnvalue.Success, returnvalue.Message, returnvalue.Data, returnvalue.TimeTaken);
+        }
+
+        private void Initialise(bool success, string message, object data, TimeSpan timeTaken)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+            TimeTaken = timeTaken;
+        }
+
+        public ReturnValue(bool success, string message, object data)
+        {
+            Initialise(success, message, data, new TimeSpan());
+        }
+    }
+}
