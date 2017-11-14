3e4ea406aae7b84318ca2132edb1235391228fe2
David Hartley <dhartley84@gmail.com>
2017-11-14 22:37:16 +1100

    Implemented PersonStatusQueryRepository

---
+++ Database/Repositories/PersonStatusQueryRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Database.Dapper;
+using Database.Interfaces.Models;
+using Database.Interfaces.Repositories;
+using Database.Sql;
+
+namespace Database.Repositories
+{
+    public class PersonStatusQueryRepository : RepositoryBase, IPersonStatusQueryRepository
+    {
+        public PersonStatusQueryRepository(IConnectionFactory connectionFactory) : base(connectionFactory)
+        {
+        }
+
+        public async Task<PersonStatus> GetAsync(int id)
+        {
+            return await QueryAsync(q => q.QueryFirstOrDefaultAsync<PersonStatus>(PersonStatusSql.GetById, new { id }));
+        }
+
+        public async Task<PersonStatus> GetAllAsync()
+        {
+            return await QueryAsync(q => q.QueryFirstOrDefaultAsync<PersonStatus>(PersonStatusSql.GetAll));
+        }
+    }
+}
---
+++ Database/Sql/PersonStatusSql.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Sql
+{
+    public static class PersonStatusSql
+    {
+        public const string GetById = "SELECT * FROM PersonStatus WHERE Id = @id";
+        public const string GetAll = "SELECT * FROM PersonStatus";
+    }
+}
