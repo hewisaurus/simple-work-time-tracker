1c9486c10cd8aa1bfecad040e79cd1019639baf5
David Hartley <dhartley84@gmail.com>
2017-11-14 22:33:03 +1100

    Added PersonStatusQueryRepository interface

---
+++ Database.Interfaces/Repositories/IPersonStatusQueryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Database.Interfaces.Models;
+using Database.Interfaces.Repositories.Base;
+
+namespace Database.Interfaces.Repositories
+{
+    public interface IPersonStatusQueryRepository : IQueryRepository<PersonStatus>
+    {
+    }
+}
