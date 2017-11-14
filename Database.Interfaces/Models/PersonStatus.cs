47673765249c6adb1be017e8332e1ca710d5be81
David Hartley <dhartley84@gmail.com>
2017-11-14 22:31:36 +1100

    Added PersonStatus

---
+++ Database.Interfaces/Models/PersonStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Database.Interfaces.Models.Base;
+
+namespace Database.Interfaces.Models
+{
+    public class PersonStatus : ModelBase
+    {
+        public string Name { get; set; }
+        public string AppearanceClass { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
